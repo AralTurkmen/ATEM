8e4a9796aab2f9457bfdcfe076b420a6c60b5fd7
EliasMilton <eliasmilton04@gmail.com>
2026-03-05 19:44:02 +0100

    MainCode - Class Hardware

--- Source Code/Hardware.java
+++ Source Code/Hardware.java
@@ -2,9 +2,21 @@ import java.time.LocalDate;
 
 Public class Hardware {
 
-    private string name;
+    private String name;
     private String manufacturer;
-    private localDate purchaseDate;
+    private int serialNumber;
+    private LocalDate purchaseDate;
 
-
+    public Hardware(String name, String manufacturer, int serialNumber, LocalDate purchasDate) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.serialNumber = serialNumber;
+        this.purchaseDate = purchaseDate;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getManufacturer(){
+        return manufacturer;
+    }
 }
